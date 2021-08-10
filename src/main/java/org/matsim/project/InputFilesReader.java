4d9c4b3b48d4c91c19632b712fdf9418da0f9723
Christopher Day <christophersday@gmail.com>
2021-08-09 20:40:09 -0600

    Add main class that creates the final population file
    This class calls upon the reader classes from commit 57a7b7 and forms a final population file for the SLC scenario. It was created and obtained in the same fashion as the previous commit .

---
+++ src/main/java/org/matsim/project/InputFilesReader.java
@@ -0,0 +1,58 @@
+package org.matsim.project;
+
+import org.activitysim.utils.ActivitySimFacilitiesReader;
+import org.activitysim.utils.ActivitySimPersonsReader;
+import org.activitysim.utils.ActivitySimTripsReader;
+import org.apache.log4j.Logger;
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.PopulationWriter;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.scenario.ScenarioUtils;
+
+import java.io.File;
+
+public class InputFilesReader {
+    private static final Logger log = Logger.getLogger(InputFilesReader.class);
+    private Scenario scenario;
+
+    public InputFilesReader(Scenario sc){
+        this.scenario = sc;
+    }
+
+    /**
+     * Read the ActivitySim output files into the scenario
+     * @param personsFile Path to activitysim output persons file
+     * @param tripsFile Path to activitysim output trips file
+     */
+    public void readActivitySimFiles(File personsFile, File tripsFile, File facilitiesFile, File householdsFile, File householdCoordFile){
+        ActivitySimFacilitiesReader facilitiesReader = new ActivitySimFacilitiesReader(scenario, facilitiesFile, householdsFile, householdCoordFile);
+        facilitiesReader.readFacilities();
+        facilitiesReader.readHouseholds();
+        ActivitySimPersonsReader personsReader = new ActivitySimPersonsReader(scenario, personsFile);
+        personsReader.readPersons();
+        ActivitySimTripsReader tripsReader = new ActivitySimTripsReader(scenario, tripsFile, facilitiesReader.getTazFacilityMap());
+        tripsReader.readTrips();
+        personsReader.readPlans();
+
+    }
+
+
+    public static void main(String[] args){
+        Scenario scenario = ScenarioUtils.createScenario(ConfigUtils.createConfig());
+        scenario.getConfig().global().setCoordinateSystem("EPSG:26912");
+        InputFilesReader reader = new InputFilesReader(scenario);
+        String scenarioPath = "scenarios/slc_beam/general/2.5k/";
+        String outputFileName = "slc_plans_2.5k_tours.xml.gz";
+
+        File personsFile = new File(scenarioPath + "persons.csv");
+        File tripsFile = new File(scenarioPath + "trips.csv");
+        File householdsFile = new File(scenarioPath + "households.csv");
+        File facilitiesFile = new File(scenarioPath + "facility_ids.csv");
+        File householdCoordFile = new File(scenarioPath + "hhcoord.csv");
+        reader.readActivitySimFiles(personsFile, tripsFile, facilitiesFile, householdsFile, householdCoordFile);
+
+        // new FacilitiesWriter(scenario.getActivityFacilities()).write(scenarioPath + "facilities.xml.gz");
+        new PopulationWriter(scenario.getPopulation()).write(scenarioPath + outputFileName);
+    }
+
+}
